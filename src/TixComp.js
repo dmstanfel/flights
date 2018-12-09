ed913b1ea0f50678a76ba19dc29bb5846d7f5998
Joseph Cox <20048981+healeyjoseph@users.noreply.github.com>
2018-12-09 15:18:00 -0500

    add filtcomp and tixcomp, css

---
+++ src/TixComp.js
@@ -0,0 +1,19 @@
+﻿import React, {Component} from 'react';
+import './TixComp.css';
+
+class TixComp extends Component{
+
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return (
+            <div className="ticket"> 
+                <p> ticket x x x x x x x x x x x x x x x x x x x x x x x x</p>
+            </div>
+        );
+    }
+}
+export default TixComp;
