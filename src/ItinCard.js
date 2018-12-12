67f9905c8004be100e9feeaf8de3f3e741d6272b
David Stanfel <dmstanfel@gmail.com>
2018-12-11 20:06:57 -0500

    changes merge

--- src/ItinCard.js
+++ src/ItinCard.js
@@ -1,26 +1,22 @@
 import React, {Component} from 'react';
 import './ItinPage.css';
-
-class ItinCard extends Component{
+ class ItinCard extends Component{
   constructor(props){
     super(props);
     this.state = {}
   }
-
-  render() {
+   render() {
     return (
       <>
         <h2>{this.props.depAirport} to {this.props.arrAirport}</h2>
         <h3 className="subtitle">{this.props.airline}</h3>
-        <h5> Leaving</h5>
         <p>{this.props.depDate}</p>
+        <h5> Departing</h5>
         <p>{this.props.depTime}</p>
-        <h5> Returning</h5>
-        <p>{this.props.arrDate}</p>
+        <h5> Arriving</h5>
         <p>{this.props.arrTime}</p>
       </>
     )
   }
 }
-
-export default ItinCard
+ export default ItinCard
--- src/ItinPage.js
+++ src/ItinPage.js
@@ -3,16 +3,14 @@ import axios from 'axios';
 import {API_URL} from './Const';
 import './ItinPage.css';
 import ItinCard from './ItinCard.js'
-
-class ItinPage extends Component{
+ class ItinPage extends Component{
   constructor(props){
     super(props);
     this.state = {flights:[], airlines:[],
                   airports:[], cumulativeTags: [],
-                  removed: []}
+                  removed: [], tickets:[]}
   }
-
-  // Here we just fetch the first 10 flights, but actually we should
+   // Here we just fetch the first 10 flights, but actually we should
   // fetch saved flights. Fetching airlines and airports is still required
   // I believe...
   componentDidMount(){
@@ -22,6 +20,15 @@ class ItinPage extends Component{
       url: API_URL + 'flights'
     }).then((response) => {
       this.setState({flights: response.data});
+    }).catch((error) => {
+      console.log(error);
+    });
+    axios({
+      method: 'get',
+      withCredentials: true,
+      url: API_URL + 'tickets'
+    }).then((response) => {
+      this.setState({tickets: response.data});
     }).catch((error) => {
       console.log(error);
     });
@@ -44,30 +51,32 @@ class ItinPage extends Component{
       console.log(error);
     });
   }
-
-  findCorrespondingAirline(id) {
+   findCorrespondingAirline(id) {
     let airline = this.state.airlines.find(function(airlineIndiv) {
       return airlineIndiv.id === id
     })
     return airline.name
   }
-
-  findCorrespondingAirport(id) {
+   findCorrespondingAirport(id) {
     let airport = this.state.airports.find(function(element) {
       return element.id === id
     })
     return airport.code
   }
-
-  makeTimeNormalAgain(abnormalTime) {
+   findCorrespondingFlight(id) {
+    let flight = this.state.flights.find(function(element) {
+      return element.id === id
+    })
+    return flight
+  }
+   makeTimeNormalAgain(abnormalTime) {
     let dates = abnormalTime.split('-')
     let rest = dates[2].split('T')
     let times = rest[1].split(':')
     let amPm = "AM"
     let hour = times[0]
     let minute = times[1]
-
-    hour = parseInt(hour)
+     hour = parseInt(hour)
     if (hour >= 12) {
       amPm = "PM"
     }
@@ -78,24 +87,26 @@ class ItinPage extends Component{
       hour = 12
     }
     let normalTime = hour + ":" + minute + " " + amPm
-    let normalDate = rest[0] + "/" + dates[1] + "/" + dates[0]
-    let normalness = [normalDate,normalTime]
-    return normalness
+    return normalTime
   }
-
-  // Will need to add backend removal of the saved flight as well.
+   // Will need to add backend removal of the saved flight as well.
   // This will allow for the card not to be rendered initially, but I think,
   // If the backend is not hooked up, then the card will reappear on page
   // refresh. So if we add backend stuff, it should work properly.
   //
   // Also this is very hacky.
   removeCard = (i) => {
-    let addTo = parseInt(i.currentTarget.id)
+    let id = i.currentTarget.id.split(' ')
+    let addTo = parseInt(id[0])
     let newRem = this.state.removed.concat(addTo)
     this.setState({removed: newRem})
+    axios({
+        method: 'DELETE',
+        withCredentials: true,
+        url: API_URL + 'tickets/' + id[1]
+    })
   }
-
-  renderTenIds(){
+   renderTenIds(){
     let cumulativeTag = []
     // Poor way to get around this loading issue...
     if (this.state.flights.length === 0
@@ -104,14 +115,14 @@ class ItinPage extends Component{
       return <p> Loading... </p>
     } else {
       let i = 0
-      for (i = 0; i < 10; i++) {
-        let depAirport = this.findCorrespondingAirport(this.state.flights[i].departure_id)
-        let arrAirport = this.findCorrespondingAirport(this.state.flights[i].arrival_id)
-        let departing = this.makeTimeNormalAgain(this.state.flights[i].departs_at)
-        let arriving = this.makeTimeNormalAgain(this.state.flights[i].arrives_at)
-        let airline = this.findCorrespondingAirline(this.state.flights[i].airline_id)
-
-        // Delete Button could use more formatting.
+      for (i = 0; i < this.state.tickets.length; i++) {
+        let flight = this.findCorrespondingFlight(this.state.tickets[i].age)
+        let depAirport = this.findCorrespondingAirport(flight.departure_id)
+        let arrAirport = this.findCorrespondingAirport(flight.arrival_id)
+        let departing = this.makeTimeNormalAgain(flight.departs_at)
+        let arriving = this.makeTimeNormalAgain(flight.arrives_at)
+        let airline = this.findCorrespondingAirline(flight.airline_id)
+         // Delete Button could use more formatting.
         // ItinCard it's own component, could do flipping inside the ItinCard
         // class... More thoughts on that at a later time.
         if (!this.state.removed.includes(i)) {
@@ -122,13 +133,12 @@ class ItinPage extends Component{
               depAirport={depAirport}
               arrAirport={arrAirport}
               airline={airline}
-              depDate={departing[0]}
-              depTime={departing[1]}
-              arrDate={arriving[0]}
-              arrTime={arriving[1]}
-              flight={this.state.flights[i].plane_id}
+              depDate={this.state.tickets[i].gender}
+              depTime={departing}
+              arrTime={arriving}
+              flight={flight.plane_id}
               />
-              <button id={i} onClick={this.removeCard}> Delete </button>
+              <button id={i + " " + this.state.tickets[i].id} onClick={this.removeCard}> Delete </button>
             </div>
           )
         }
@@ -136,8 +146,7 @@ class ItinPage extends Component{
     }
     return cumulativeTag;
   }
-
-  render(){
+   render(){
     let cumulativeTag = this.renderTenIds()
     return(
       <>
